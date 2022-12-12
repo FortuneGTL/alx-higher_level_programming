a3f2a371bea62fc08a1b339c07c7ffb54fa34720
Fortunatus Azobua <fortunatusazobua@gmail.com>
2022-12-11 22:27:32 -0800

    Corrected all

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    return {k: v * 2 for k, v in a_dictionary.items()}
+    new_dictionary = {}
 
+    for key in a_dictionary:
+        new_dictionary[key] = a_dictionary[key] * 2
+    return new_dictionary
